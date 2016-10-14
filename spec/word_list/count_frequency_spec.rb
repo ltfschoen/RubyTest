7b63a3de198b8712e47551170cb7b5272d6b5a19
Luke Schoen <ltfschoen@gmail.com>
2016-10-14 11:21:52 +1100

    Sort by top 3 frequencies

--- spec/word_list/count_frequency_spec.rb
+++ spec/word_list/count_frequency_spec.rb
@@ -11,9 +11,14 @@ RSpec.describe WordList, "#count_frequency" do
 
   context "with word list" do
     let(:expected_frequency_count) { {"array"=>1, "of"=>2, "multiple"=>1, "string"=>2, "values"=>1, "from"=>1, "words"=>1} }
+    let(:expected_top_three) { [["of", 2], ["string", 2], ["values", 1]] }
 
     it "counts frequency of duplicate words correctly" do
       expect(@word_list.count_frequency).to eq expected_frequency_count
+    end
+
+    it "returns top 3 frequency counts in descending order" do
+      expect(@word_list.top_three).to eq expected_top_three
     end
   end
 end
--- word_list/word_list.rb
+++ word_list/word_list.rb
@@ -9,18 +9,29 @@ class WordList
     self.count_frequency = word_list
   end
 
+  public
+
+    # iterate over array with minimal coupling
+    def top_three
+      sorted = @count_frequency.sort_by{|word, count| count}.reverse
+      top_three = sorted.first(3)
+      top_three.each do |word, count|
+        puts "#{word}: #{count}"
+      end
+    end
+
   private
 
-  # Setter method enforces rules
-  def word_list=(word_list)
-    @word_list = word_list
-  end
-
-  def count_frequency=(word_list)
-    counts = Hash.new(0)
-    for word in word_list
-      counts[word] += 1
+    # Setter method enforces rules
+    def word_list=(word_list)
+      @word_list = word_list
     end
-    @count_frequency = counts
-  end
+
+    def count_frequency=(word_list)
+      counts = Hash.new(0)
+      for word in word_list
+        counts[word] += 1
+      end
+      @count_frequency = counts
+    end
 end
