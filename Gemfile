95b99e29d150000c70876ca4d471bb128ec809db
Luke Schoen <ltfschoen@gmail.com>
2016-10-18 21:24:44 +1100

    Added FactoryGirl. Refactored

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ source "https://rubygems.org"
 gem 'activemodel', '~> 5.0.0.1'
 gem 'bigdecimal', '~> 1.2.7'
 
-group :development do
+group :development, :test do
   gem 'rspec', '~> 3.6.0.beta1', :require => false
+  gem 'factory_girl', '~> 4.7.0', :require => false
 end
--- team_system/team.rb
+++ team_system/team.rb
@@ -1,57 +1,48 @@
 class Team
 
-  attr_reader :uid, :size, :rating_total, :rating_count, :team_groups
+  attr_reader :uid, :size, :rating_total, :rating_change_count, :team_groups
 
   def initialize(**data)
     # instance attribute cannot be id and assigned nil
-    data.has_key?(:uid)
-    is_valid(data[:uid])
-    @uid = data.has_key?(:uid) && is_valid(data[:uid]) ? data[:uid] : self.object_id
-    @size = data[:size]
-    @team_groups = Hash.new(0)
-    @rating_total = @rating_count = 0
+    self.uid = data.has_key?(:uid) && is_valid(data[:uid]) ? data[:uid] : self.object_id
+    self.size = data[:size]
+    self.team_groups = Hash.new(0)
+    self.rating_total = self.rating_change_count = 0
   end
 
   public
 
+  def uid=(uid); @uid = uid end
+  def size=(size); @size = size end
+  def team_groups=(team_groups); @team_groups = team_groups end
+  def rating_total=(rating_total); @rating_total = rating_total end
+  def rating_change_count=(rating_change_count); @rating_change_count = rating_change_count end
+
   def <<(new_rating)
     @rating_total += new_rating
-    @rating_count += 1
+    @rating_change_count += 1
     self # allow method chaining
   end
 
   def rating_average
-    fail "No ratings" if @rating_count.zero?
-    Float(@rating_total) / @rating_count
+    fail "No ratings" if @rating_change_count.zero?
+    Float(@rating_total) / @rating_change_count
   end
 
   # params are team ids to assign to team name stored in last element
-  def []=(*params)
-    @team_groups[params.pop.to_sym] = params
-  end
+  def []=(*params); @team_groups[params.pop.to_sym] = params end
 
   private
 
-  def self.all
-    ObjectSpace.each_object(Team).to_a
-  end
+  def self.all; ObjectSpace.each_object(Team).to_a end
 
-  def all
-    Team.all
-  end
+  def all; Team.all end
 
   def has_instance_with_id(uid)
-    a = all
-    a.detect { |obj|
-      if !obj.uid.nil?
-        obj.uid == uid
-      end
-    }
+    all.detect { |obj| if !obj.uid.nil?; obj.uid == uid end }
   end
 
-  def is_valid(uid)
-    !uid.nil? && has_instance_with_id(uid).nil?
-  end
+  def is_valid(uid); !uid.nil? && has_instance_with_id(uid).nil? end
 end
 
 # team1 = Team.new(uid: nil, size: 100)
@@ -59,6 +50,6 @@ end
 # team3 = Team.new(uid: 200, size: 300)
 # team4 = Team.new(uid: 200, size: 400)
 # team5 = Team.new(size: 500)
-team6 = Team.new(uid: 100, size: 200)
-team6 << 10 << 20 << 30 << 40
-team6['444', '555'] = :super_team_name
+# team6 = Team.new(uid: 100, size: 200)
+# team6 << 10 << 20 << 30 << 40
+# team6['444', '555'] = :super_team_name
