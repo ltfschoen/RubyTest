01246ab0fe2f78330a7184f0aa3303055eba9b69
Luke Schoen <ltfschoen@gmail.com>
2016-10-30 07:11:43 +1100

    Added edge cases for refactor string code test

--- spec/code_test/test_spec.rb
+++ spec/code_test/test_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Reformat, "#test" do
     @s1 = "00-44  48 5555 8361" # expected result "004-448-555-583-61"
     @s2 = "0 - 22 1985--324" # expected result "022-198-53-24"
     @s3 = "555372654" # expected result "555-372-654"
+    @s4 = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    # edge cases
+    @s5 = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111119"
+    @s6 = "1"
   end
 
   context "with sample unformatted strings" do
@@ -20,6 +24,18 @@ RSpec.describe Reformat, "#test" do
 
     it "reformats string sample 3 into expected result" do
       expect(solution(@s3)).to eq "555-372-654"
+    end
+
+    it "reformats string sample 4 into expected result" do
+      expect(solution(@s4)).to eq "111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-111-11-11"
+    end
+
+    it "raises runtime error when input >= 100 characters" do
+      expect{ solution(@s5) }.to raise_error(RuntimeError)
+    end
+
+    it "raises runtime error when input <= 2 characters" do
+      expect{ solution(@s6) }.to raise_error(RuntimeError)
     end
   end
 end
