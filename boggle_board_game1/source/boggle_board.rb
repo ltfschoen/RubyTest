35983de730a800e2dbef9724f28c21e903c9855d
Luke Schoen <ltfschoen@gmail.com>
2017-09-12 14:47:56 +1000

    Generate 4x4 board with blanks

--- boggle_board_game1/source/boggle_board.rb
+++ boggle_board_game1/source/boggle_board.rb
@@ -7,8 +7,19 @@ class BoggleBoard
   end
 
   def to_s
-    print @space
+    # create a new variable and assign an empty string to store output
+    output_string = String.new # equivalent to `output_string = ""`
+    # store the instance variable containing the array in a temporary variable `letter_array`
+    cols = rows = Math.sqrt(@space.length).to_i
+    letter_array = @space
+    # iterate over the array
+    rows.times do
+      output_string << letter_array.shift(4).join("") + "\n"
+    end
+    # last line of code in a Ruby method is returned
+    output_string
   end
 end
 
-puts BoggleBoard.new
+board = BoggleBoard.new
+puts board.to_s
